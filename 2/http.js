9474c5a3518d1b97ef516ed92f5f6377bff1e2a0
cz <1592528567@qq.com>
2019-02-16 18:48:14 +0800

    add text

--- 2/http.js
+++ 2/http.js
@@ -1,7 +1,7 @@
 var http=require('http');
 http.createServer(function(req,res){
     // 发送HTTP头部
-    
+    console.log(req.url)
     res.writeHead(200,{"Content-Type":"text/html;charset=utf-8"})
     res.write("你好 node.js")
     res.end()
